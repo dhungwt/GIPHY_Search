cd6c4b896b1af5103216cb6f1125d1f342e9e33e
dhungwt <112122672+dhungwt@users.noreply.github.com>
2023-06-18 23:52:53 -0400

    updated app.js and searchfield

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect } from "react";
 
 function App() {
+  const isLoaded = true; //tracks whether or not page loaded succeffully
+  const [gifs, setGifs] = useState([]); //
+
+  const handleSearch = () => {
+    //function returns gifs after input submitted
+    //Regular Search: http://api.giphy.com/v1/gifs/search?q=SEARCH+TERM+GOES+HERE&api_key=YOUR_API_KEY
+    setGifs();
+  };
+
+  //useEffect that loads trending gifs once the page loads
+  useEffect(() => {
+    loadTrending();
+    //fetch gifs from GIPHY
+    //Trending Search: http://api.giphy.com/v1/gifs/trending?api_key=YOUR_API_KEY
+  }, []);
+  //square brackets on line 16 specify condition that will make useEffect run
+  //here they are empty b/c loadTrending will load trending gifs once when the component is first loads
+
+  const loadTrending = () => {};
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"></header>
+      //HTML of the app
+      <h1 className="nav">Diana Hung's GIPHY API App</h1>
     </div>
   );
 }
--- src/Components/Gifcard.jsx
+++ src/Components/Gifcard.jsx
@@ -3,7 +3,8 @@ import React, { useEffect, useState } from "react";
 
 //component for individual gifs and how they appear on the page
 const Gifcard = ({ gif }) => {
-  return ( //displays gifs at their default height
+  return ( 
+    //displays gifs 
     <div class="cards">
       <img src={gif.images.fixed_height.url}/>
     </div>
