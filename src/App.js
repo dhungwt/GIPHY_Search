18a74765db1bf2d86171b2ca21cf9c01c2918533
dhungwt <112122672+dhungwt@users.noreply.github.com>
2023-06-19 10:23:51 -0400

    attempting to write searchfield differently, error in return

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import "./App.css";
 import { useEffect } from "react";
 
 function App() {
-  const isLoaded = true; //tracks whether or not page loaded succeffully
+  const [loaded, setLoaded] = true; //tracks whether or not page loaded succeffully
   const [gifs, setGifs] = useState([]); //
+  const [input, setInput] = useState(""); //state that stores user's input in search
+  //default is an empty string
 
   const handleSearch = () => {
     //function returns gifs after input submitted
@@ -16,12 +18,31 @@ function App() {
   useEffect(() => {
     loadTrending();
     //fetch gifs from GIPHY
-    //Trending Search: http://api.giphy.com/v1/gifs/trending?api_key=YOUR_API_KEY
   }, []);
-  //square brackets on line 20 specify condition that will make useEffect run
+  //square brackets above specify condition that will make useEffect run
   //here they are empty b/c loadTrending will load trending gifs once when the component is first loads
 
-  const loadTrending = () => {};
+  const loadTrending = () => { //function that loads trending gifs
+    setLoaded(true);
+    try{ //try catch for retreival of trending gifs
+    const result = await axios.get(
+      `http://api.giphy.com/v1/gifs/trending?api_key=ptLg3iWyFUnQLRh4KmRvlxEBtdswbnMM`
+    );
+    setGifs(result.data.data); //gifs are returned by GIPHY as an array of gifs in the JSON
+    setLoading(false); //set back to false after trending gifs load
+  };
+}catch{
+ console.error(("An error occured while loading treanding gifs:", error);
+ setLoading(false); //set back to false after error encountered
+}
+
+async function fetchGifs(){
+  setLoaded(true);
+  const result = await axios.get(`https://zip-api.eu/api/v1/info/US-${input}`) 
+  console.log(result)
+  setGifs(result.data.data); 
+  setLoading(false);
+}
 
   return (
     <div className="App">
@@ -29,6 +50,12 @@ function App() {
       //HTML of the app
       <h1 className="nav">Diana Hung's GIPHY API App</h1>
       <searchfield/>
+      {/*following will display trending when first loaded and results if an input is submitted*/}
+      {loaded ? (
+        loadTrending;
+      ) : (
+        gifs.map((gif) => <Gifcard key={gif.id} gif={gif} />) //each gif returned in the array is displayed in its own Gifcard
+      )}
       <gifcard />
     </div>
   );
